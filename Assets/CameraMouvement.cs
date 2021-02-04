82012a261a05132b289905d9beb1a8c68336ac00
Kasimi Karim <karim.kasimi@etudiant.univ-rennes1.fr>
2021-02-04 12:31:05 +0100

    bon avancemen

---
+++ Assets/CameraMouvement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouvement : MonoBehaviour
+{
+    public GameObject corps;
+
+    public Rigidbody rbCamera;
+
+    public Boolean declenchement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       corps =  GameObject.Find("corps");
+       rbCamera = gameObject.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.LookAt(corps.transform);
+        if (declenchement)
+        {
+            rbCamera.AddForce(corps.transform.position - gameObject.transform.position);
+            rbCamera.AddForce(-rbCamera.velocity * 0.8f);
+        }
+
+    }
+}
---
+++ Assets/Mouvements.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouvements : MonoBehaviour
+{
+    public Rigidbody rb;
+    public BoxCollider bc;
+    public CameraMouvement scriptCamera;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        bc = gameObject.GetComponent<BoxCollider>();
+        scriptCamera = GameObject.Find("Main Camera").GetComponent<CameraMouvement>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On augmente la friction si l'user appuye sur espace =  freinage
+
+
+        if (Input.GetKeyDown("space"))
+        {
+
+            bc.material.dynamicFriction = 1.0f;
+            
+           
+
+
+            // On diminue la friction si l'user appuye sur espace = propulsion
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            bc.material.dynamicFriction = 0.0f;
+            rb.AddForce(transform.forward * 1000.0f);
+            scriptCamera.declenchement = true;
+        }       
+        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+           
+            rb.AddForce(transform.right * 1000.0f);
+         
+           
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rb.AddForce(transform.right * - 1000.0f);
+        }
+    }
+}
